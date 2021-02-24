b9368fb3e769334b4a5bd2b2d9bb650f443e2c5f
virtuallight <etzlanim@gmail.com>
2021-02-24 13:26:51 -0500

    Update heatmap_utils.py

--- heatmap_utils.py
+++ heatmap_utils.py
@@ -1,3 +1,4 @@
+import sys
 import pandas as pd
 import numpy as np
 import seaborn as sns
@@ -5,15 +6,8 @@ import matplotlib.pyplot as plt
 
 
 def plot_heatmap(file, meta_file, out_dir, p_value_threhold, q_value_threhold,
-                 dpsi_threshold, foldchange_threshold, avg_threshold, aggregate,
-                 method, metric, prefix):
-    def process_group(x):
-        start, end = float('inf'), 0
-        for label in x['FeatureLabel']:
-            _chr, coord = label.split(':')
-            _start, _end = (int(v) for v in coord.split('-'))
-            start, end = min(start, _start), max(end, _end)
-        return f"{x['GeneName'].iloc[0]}_{_chr}:{start}-{end}"
+                 dpsi_threshold, foldchange_threshold, avg_threshold,
+                 unsupervised, aggregate, method, metric, prefix, top):
 
     samples = []
     conditions = []
@@ -26,49 +20,138 @@ def plot_heatmap(file, meta_file, out_dir, p_value_threhold, q_value_threhold,
             sample_cond_dict[sample] = cond
 
     data_df = pd.read_csv(file, sep='\t', comment='#')
+    original_columns = data_df.columns
+    if np.all(data_df['ReadCount1'] == '.') and np.all(data_df['PSI'] == '.'):
+        raise Exception("Column 'ReadCount1' and 'PSI' don't contain values! This could happen if the TSV file is generated from MAJIQ outputs.")
+        sys.exit('-1')
+
+    if unsupervised:
+        data_df = process_data_unsupervised(data_df, samples, original_columns, avg_threshold, aggregate, top)
+    else:
+        data_df = process_data_supervised(data_df, samples, sample_cond_dict, conditions, original_columns,
+                                          p_value_threhold, q_value_threhold, dpsi_threshold, foldchange_threshold,
+                                          avg_threshold, aggregate)
+
+    generate_heatmaps(data_df, original_columns, conditions, sample_cond_dict,
+                      method, metric, prefix, aggregate, unsupervised, out_dir)
+
+
+def process_group(x):
+    start, end = float('inf'), 0
+    for label in x['FeatureLabel']:
+        _chr, coord = label.split(':')
+        _start, _end = (int(v) for v in coord.split('-'))
+        start, end = min(start, _start), max(end, _end)
+    return f"{x['GeneName'].iloc[0]}_{_chr}:{start}-{end}"
+
+
+def process_data_unsupervised(data_df, samples, original_columns, avg_threshold, aggregate, top_num):
+    print('Unsupervised mode, the option --q-value, --p-value, --dpsi, --fold-change will be ignored')
+    if 'dPSI' in original_columns:
+        if data_df.shape[1] > 14 and aggregate:
+            print("Multi-way comparison doesn't support aggregate mode, aggregate mode disabled!")
+            aggregate = False
+
+        if aggregate:
+            data_df2 = data_df['PSI'].str.split(',', expand=True).astype(float)
+            data_df2.columns = samples
+            data_df2[['GeneName', 'FeatureLabel', 'GroupID', 'dPSI']] = data_df[['GeneName', 'FeatureLabel', 'GroupID', 'dPSI']]
+
+            groups = data_df2.groupby(['GroupID'])
+            data_df3 = groups.apply(lambda x: x.iloc[x[samples[0]].argmax()])
+            data_df3['Bool'] = data_df3['dPSI'].apply(lambda x: 1 if x > 0 else -1)
+            data_df4 = pd.merge(data_df2, data_df3['Bool'], on='GroupID')
+            data_df4 = data_df4[data_df4['dPSI'] * data_df4['Bool'] > 0]
+
+            groups = data_df4.groupby(['GroupID'])
+            data_df = groups.apply(lambda x: np.sum(x[samples], axis=0))
+            data_df.index = groups.apply(process_group)
+        else:
+            data_df2 = data_df['PSI'].str.split(',', expand=True).astype(float)
+            data_df2.columns = samples
+            data_df2['variance'] = data_df2.mean(axis=1) / data_df2.var(axis=1)
+            data_df2['index'] = data_df[['GeneName', 'FeatureLabel']].agg('_'.join, axis=1)
+
+            groups = data_df2.groupby(['index'])
+            data_df = groups.apply(lambda x: x.iloc[x['variance'].argmax()])
+            data_df = data_df.sort_values(by=['variance'], ascending=False)
+            data_df = data_df.drop(columns=['variance', 'index'])
+
+    elif 'log2FoldChange' in original_columns:
+        data_df = data_df[data_df.iloc[:, 12:].apply(lambda x: np.any(x > avg_threshold) ,axis=1)]
+        data_df2 = data_df['ReadCount1'].str.split(',', expand=True).astype(float)
+        data_df2.columns = samples
+        data_df2['variance'] = data_df2.mean(axis=1) / data_df2.var(axis=1)
+        data_df2.index = data_df[['GeneName', 'FeatureLabel']].agg('_'.join, axis=1)
+        data_df = data_df2
+        data_df = data_df.sort_values(by=['variance'], ascending=False)
+        data_df = data_df.drop(columns=['variance'])
+
+    num = min(top_num, data_df.shape[0])
+    data_df = data_df.iloc[:num, :]
+    return data_df
+
+
+def process_data_supervised(data_df, samples, sample_cond_dict, conditions, original_columns,
+                            p_value_threhold, q_value_threhold, dpsi_threshold,
+                            foldchange_threshold, avg_threshold, aggregate):
     data_df = data_df[data_df['p-value'] < p_value_threhold]
     data_df = data_df[data_df['q-value'] < q_value_threhold]
-    data_df = data_df[data_df['GeneName'] != '.']
-    original_columns = data_df.columns
+    # data_df = data_df[data_df['GeneName'] != '.']
 
     if 'dPSI' in original_columns:
         if data_df.shape[1] > 14 and aggregate:
-            print("Multi-way comparison doesn't support aggregate mode, change to non-aggregate mode instead")
+            print("Multi-way comparison doesn't support aggregate mode, aggregate mode disabled!")
             aggregate = False
 
         if aggregate:
             selected_groups = data_df[abs(data_df['dPSI']) > dpsi_threshold]['GroupID'].drop_duplicates()
             data_df = data_df[data_df['GroupID'].isin(selected_groups)]
-            data_df = data_df[data_df['dPSI'] > 0]
-            new_data_df = data_df['PSI'].str.split(',', expand=True).astype(float)
-            new_data_df.columns = samples
-            new_data_df[['GeneName', 'FeatureLabel', 'GroupID']] = data_df[['GeneName', 'FeatureLabel', 'GroupID']]
-            groups = new_data_df.groupby(['GroupID'])
+
+            data_df2 = data_df['PSI'].str.split(',', expand=True).astype(float)
+            data_df2.columns = samples
+            data_df2[['GeneName', 'FeatureLabel', 'GroupID', 'dPSI']] = data_df[['GeneName', 'FeatureLabel', 'GroupID', 'dPSI']]
+
+            groups = data_df2.groupby(['GroupID'])
+            cond1_samples = [sample for sample, cond in sample_cond_dict.items() if cond == conditions[0]]
+            data_df3 = groups.apply(lambda x: x.iloc[x[cond1_samples].max(axis=1).argmax()])
+            data_df3['Bool'] = data_df3['dPSI'].apply(lambda x: 1 if x > 0 else -1)
+            data_df4 = pd.merge(data_df2, data_df3['Bool'], on='GroupID')
+            data_df4 = data_df4[data_df4['dPSI'] * data_df4['Bool'] > 0]
+
+            groups = data_df4.groupby(['GroupID'])
             data_df = groups.apply(lambda x: np.sum(x[samples], axis=0))
             data_df.index = groups.apply(process_group)
         else:
             data_df = data_df[abs(data_df['dPSI']) > dpsi_threshold]
-            new_data_df = data_df['PSI'].str.split(',', expand=True).astype(float)
-            new_data_df.columns = samples
-            new_data_df['index'] = data_df[['GeneName', 'FeatureLabel']].agg('_'.join, axis=1)
-            new_data_df['dPSI'] = data_df['dPSI']
-            groups = new_data_df.groupby(['index'])
+            data_df2 = data_df['PSI'].str.split(',', expand=True).astype(float)
+            data_df2.columns = samples
+            data_df2['index'] = data_df[['GeneName', 'FeatureLabel']].agg('_'.join, axis=1)
+            data_df2['dPSI'] = np.abs(data_df['dPSI'])
+
+            groups = data_df2.groupby(['index'])
             data_df = groups.apply(lambda x: x.iloc[x['dPSI'].argmax()])
+            data_df = data_df.sort_values(by=['dPSI'], ascending=False)
             data_df = data_df.drop(columns=['dPSI', 'index'])
 
     elif 'log2FoldChange' in original_columns:
         data_df = data_df[foldchange_threshold < abs(data_df['log2FoldChange'])]
         data_df = data_df[abs(data_df['log2FoldChange']) < float('inf')]
         data_df = data_df[data_df.iloc[:, 12:].apply(lambda x: np.any(x > avg_threshold) ,axis=1)]
-        new_data_df = data_df['ReadCount1'].str.split(',', expand=True).astype(float)
-        new_data_df.columns = samples
-        new_data_df.index = data_df[['GeneName', 'FeatureLabel']].agg('_'.join, axis=1)
-        data_df = new_data_df
+        data_df2 = data_df['ReadCount1'].str.split(',', expand=True).astype(float)
+        data_df2.columns = samples
+        data_df2.index = data_df[['GeneName', 'FeatureLabel']].agg('_'.join, axis=1)
+        data_df = data_df2
+    return data_df
 
-    num, max_num = data_df.shape[0], 500
-    if num > max_num:
-        data_df = data_df.iloc[:max_num, :]
-        print(f'Warming: the number of selected events ({num}) are > {max_num}, will only should the first {max_num} events in the heatmaps')
+
+def generate_heatmaps(data_df, original_columns, conditions, sample_cond_dict,
+                      method, metric, prefix, aggregate, unsupervised, out_dir):
+    num = data_df.shape[0]
+    if num > 1000:
+        print('Warming: number of rows > 1000, however, Jutils only allow 1000 rows for heatmap!')
+        data_df = data_df.iloc[:1000, :]
+
     mask = data_df.isnull()
     data_df = data_df.fillna(0)
 
@@ -83,6 +166,9 @@ def plot_heatmap(file, meta_file, out_dir, p_value_threhold, q_value_threhold,
         figureWidth = 15
 
     strings = [prefix]
+    if unsupervised:
+        strings.append('unsupervised')
+
     if aggregate:
         strings.append('aggregated')
 
@@ -138,4 +224,3 @@ def set_xtick_text_colors(figure, sample_cond_dict, conditions):
     for tick_label in figure.ax_heatmap.axes.get_xticklabels():
         cond = sample_cond_dict[tick_label.get_text()]
         tick_label.set_color(cond_color_dict[cond])
-
