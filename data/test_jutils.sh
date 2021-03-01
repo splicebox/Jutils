eb3aafe79e7523125d9a201869f6dbd76ff61c9e
virtuallight <etzlanim@gmail.com>
2021-03-01 12:03:37 -0500

    Update test_jutils.sh

--- data/test_jutils.sh
+++ data/test_jutils.sh
@@ -14,8 +14,7 @@ result_dir="jutils_out"
 python3 jutils.py heatmap --tsv-file "${result_dir}/mntjulip_DSR_results.tsv" \
                           --meta-file "${base_dir}/mntjulip_meta_file.tsv" \
                           --out-dir "${out_dir}" \
-                          --p-value 0.05 --q-value 1 --dpsi 0.2 --aggregate --prefix mntjulip_DSR \
-                          --metric "cityblock" --method "average"
+                          --p-value 0.05 --q-value 1 --dpsi 0.2 --aggregate --prefix mntjulip_DSR
 
 # DSR without aggregate
 python3 jutils.py heatmap --tsv-file "${result_dir}/mntjulip_DSR_results.tsv" \
@@ -35,22 +34,21 @@ python3 jutils.py heatmap --tsv-file "${result_dir}/mntjulip_DSR_results.tsv" \
                           --meta-file "${base_dir}/mntjulip_meta_file.tsv" \
                           --out-dir "${out_dir}" \
                           --aggregate --prefix mntjulip_DSR \
-                          --unsupervised\
-                          --metric "cityblock" --method "average"
+                          --unsupervised
 
 # DSR without aggregate, unsupervised
 python3 jutils.py heatmap --tsv-file "${result_dir}/mntjulip_DSR_results.tsv" \
                           --meta-file "${base_dir}/mntjulip_meta_file.tsv" \
                           --out-dir "${out_dir}" \
-                          --unsupervised \
-                          --prefix mntjulip_DSR
+                          --prefix mntjulip_DSR \
+                          --unsupervised
 
 # DSA, unsupervised
 python3 jutils.py heatmap --tsv-file "${result_dir}/mntjulip_DSA_results.tsv" \
                           --meta-file "${base_dir}/mntjulip_meta_file.tsv" \
                           --out-dir "${out_dir}" \
-                          --unsupervised \
-                          --prefix mntjulip_DSA
+                          --prefix mntjulip_DSA \
+                          --unsupervised
 
 # Venn Diagram
 echo "${result_dir}/mntjulip_DSR_results.tsv" > ${result_dir}/tsv_file_list.txt
@@ -67,19 +65,26 @@ rm -rf ${result_dir}/tsv_file_list.txt
 echo "${result_dir}/mntjulip_DSR_results.tsv\tMntJULiP\t'--p-value 0.05 --q-value 1 --dpsi 0.05'" > ${result_dir}/tsv_file_list.txt
 echo "${result_dir}/leafcutter_results.tsv\tLeafCutter\t'--p-value 0.05 --q-value 1 --dpsi 0.05'" >> ${result_dir}/tsv_file_list.txt
 echo "${result_dir}/majiq_results.tsv\tMAJIQ\t'--p-value 0.05 --q-value 1 --dpsi 0.05'" >> ${result_dir}/tsv_file_list.txt
-echo "${result_dir}/rmats_ReadsOnTargetAndJunctionCounts_results.tsv\trMATs\t--p-value 0.05 --q-value 1 --dpsi 0.05" >> ${result_dir}/tsv_file_list.txt
+echo "${result_dir}/rmats_ReadsOnTargetAndJunctionCounts_results.tsv\trMATs\t'--p-value 0.05 --q-value 1 --dpsi 0.05'" >> ${result_dir}/tsv_file_list.txt
 
 python3 jutils.py venn-diagram --tsv-file-list "${result_dir}/tsv_file_list.txt" \
                                --out-dir "${out_dir}"
 
 
+# custom option for each tools
+rm -rf ${result_dir}/tsv_file_list.txt
+echo "${result_dir}/mntjulip_DSR_results.tsv\tMntJULiP1\t'--p-value 0.05 --q-value 1 --dpsi 0.05'" > ${result_dir}/tsv_file_list.txt
+echo "${result_dir}/mntjulip_DSR_results.tsv\tMntJULiP2\t'--p-value 0.05 --q-value 1 --dpsi 0.1'" >> ${result_dir}/tsv_file_list.txt
+echo "${result_dir}/mntjulip_DSR_results.tsv\tMntJULiP3\t'--p-value 0.05 --q-value 1 --dpsi 0.2'" >> ${result_dir}/tsv_file_list.txt
+
+python3 jutils.py venn-diagram --tsv-file-list "${result_dir}/tsv_file_list.txt" \
+                               --out-dir "${out_dir}"
+
 # Sashimi Plot
 # MntJULiP: (used g006855 for jutils paper)
-wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M17/gencode.vM17.annotation.gtf.gz
-gunzip gencode.vM17.annotation.gtf.gz
 python3 jutils.py sashimi --tsv-file "${result_dir}/mntjulip_DSR_results.tsv" \
                           --meta-file "${base_dir}/mntjulip_meta_file.tsv" \
                           --out-dir "${out_dir}" \
                           --group-id "g006855" \
-                          --gtf "gencode.vM17.annotation.gtf" \
+                          --gtf "{base_dir}/gencode.vM17.annotation.clean.gtf" \
                           --shrink
